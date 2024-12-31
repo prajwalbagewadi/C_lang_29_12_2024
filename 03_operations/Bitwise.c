490416c857d493773f7ab39e97f094cbdd1ad305
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2024-12-31 12:09:57 +0530

    operators Relational

--- 03_operations/Bitwise.c
+++ 03_operations/Bitwise.c
@@ -5,4 +5,47 @@
  * ~ Complement
  * << leftshift
  * >> rightshift
+ * &
+ * a b &
+ * 0 0 0
+ * 0 1 0
+ * 1 0 0
+ * 1 1 1
+ * 
+ * |
+ * a b |
+ * 0 0 0 
+ * 0 1 1 
+ * 1 0 1
+ * 1 1 1
+ * 
+ * ^
+ * a b ^
+ * 0 0 0
+ * 0 1 1
+ * 1 0 1
+ * 1 1 0
+ * 
+ * ~
+ * a ~
+ * 0 1
+ * 1 0
  */
+
+#include<stdio.h>
+int main(){
+    int a=5; // 0101
+    int b=10; // 1010
+    //&
+    printf("a&b=%d\n",a&b);
+
+    //|
+    printf("a|b=%d\n",a|b);
+
+    //^
+    printf("a^b=%d\n",a^b);
+
+    //~
+    printf("~a=%d\n",~a); //unary operator works with single var or operand
+    return 0;
+}
--- 03_operations/Logical.c
+++ 03_operations/Logical.c
@@ -2,4 +2,35 @@
  * && And
  * || OR
  * !  Not Reverse
+ * 
+ * &&
+ * a b &&
+ * 0 0 0
+ * 0 1 0 
+ * 1 0 0 
+ * 1 1 1
+ * 
+ * ||
+ * a b ||
+ * 0 0 0
+ * 0 1 1
+ * 1 0 1
+ * 1 1 1 
+ * 
  */
+
+#include<stdio.h>
+int main(){
+
+    int num1=10;
+    int num2=20;
+    int num3=10;
+
+    //&&
+    printf("num1!=num2 && num1<num2 :%d\n",num1!=num2 && num1<num2);
+    //||
+    printf("num1>num2 || num1<num2 :%d\n",num1>num2 || num1<num2);
+    //!
+    printf("!(num1!=num2 && num1<num2) :%d\n",!(num1!=num2 && num1<num2));
+    return 0;
+}
