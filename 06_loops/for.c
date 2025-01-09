f942a4ebaa3b4a43382146e5192fd72c105dd0b4
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-01-09 10:56:34 +0530

    for loop

---
+++ 06_loops/for.c
@@ -0,0 +1,17 @@
+//for loop
+#include<stdio.h>
+int main(){
+    // syntax:
+    // for(initilization;stopping_condition;increament/decreament){
+    //     //stmts
+    // }
+
+    for(int i=0;i<10;i++){
+        printf("loop inc=%d\n",i);
+    }
+    //tables
+    for(int i=1;i<=10;i++){
+        printf("%d\n",2*i);
+    }
+    return 0;
+}
