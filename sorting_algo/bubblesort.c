b8c45e75cb55d696d7090657292ae9a5b7e94464
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2025-01-15 12:32:20 +0530

    Update bubblesort.c

--- sorting_algo/bubblesort.c
+++ sorting_algo/bubblesort.c
@@ -6,9 +6,10 @@ int main(){
         printf("%d ",arr[i]);
     }
     printf("\n");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
+    for(int i=0;i<n;i++){ // passes
+        for(int j=0;j<n-i-1;j++){ //for unsorted elements
             if(arr[j]>arr[j+1]){
+               //swapping
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
