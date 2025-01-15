a00257f0ee1a5a3cc687ecf841d33f8ab35c6c51
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-01-15 11:40:37 +0530

    bubblesort

---
+++ sorting_algo/bubblesort.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={9,12,4,3,1};
+    int n=5;
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
