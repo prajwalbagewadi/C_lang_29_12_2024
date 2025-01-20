145f01cba3fb39605fbc28a505913d04fadff986
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-01-20 10:43:14 +0530

    functions

--- 09_functions/userdef.c
+++ 09_functions/userdef.c
@@ -3,11 +3,19 @@
 //syntax:
 //return-type function-name();
 void Greet(); //function header
+void addition();
+void add(int x,int y);
 
 int main(){
     //syntax:
     //function name();
     Greet(); //function call;
+    int a=40,b=50;
+    addition();
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    add(a,b);//actual parameters
+    add(50,50);
     return 0;
 }
 
@@ -18,4 +26,13 @@ int main(){
 //}
 void Greet(){ //function defination
     printf("Good Morning\n");
+}
+void addition(){
+    int a=10,b=20;
+    printf("addition=%d\n",(a+b));
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+}
+void add(int x,int y){//formal parameters
+    printf("add=%d\n",(x+y));
 }
